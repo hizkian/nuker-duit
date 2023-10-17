eb2febaaf656c66966c909efb1299b426cad4eed
Eki <hizkia.nugroho@pinhome.id>
2023-10-17 18:44:38 +0700

    feat: add migration and model for currencies and exchange rates

---
+++ nuker-duit-api/database/migrations/2023_10_17_113840_create_exchange_rates_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('exchange_rates', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+            $table->timestamp('deleted_at');
+            $table->integer('currency_id');
+            $table->float('idr_rate');
+
+            $table->foreign('currency_id')->references('id')->on('currencies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('exchange_rates');
+    }
+};
